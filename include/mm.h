0a2c588398c2b27bcd2e7da965d6126934f13cfe
xlzhang <wszxl516@163.com>
2023-03-07 18:08:55 +0800

    linker script add stack, mm_set, heap.

---
+++ include/mm.h
@@ -0,0 +1,10 @@
+#ifndef __MM_H__
+#define __MM_H__
+typedef void (ld_script_pointer_t)();
+extern ld_script_pointer_t stack_top, stack_bottom;
+extern ld_script_pointer_t bss_start, bss_end;
+extern ld_script_pointer_t mm_set_start, mm_set_end;
+extern ld_script_pointer_t heap_start;
+
+
+#endif //__MM_H__
--- src/main.c
+++ src/main.c
@@ -3,7 +3,8 @@
 #include "uart.h"
 #include "printf.h"
 #include "cpu.h"
- 
+#include "mm.h"
+
 void main(void) {
 	uart_init();
 	pr_notice(BOOT_LOGO "\n");
@@ -13,6 +14,10 @@ void main(void) {
 	machine_extensions(extensions);
 	pr_info("CPU Extensions: %s\n", extensions);
 	pr_info("riscv%u, Core: %d\n", machine_bits(), current_core());
+	pr_info("stack: 0x%x - 0x%x\n",stack_top, stack_bottom);
+	pr_info("bss: 0x%x - 0x%x\n",bss_start, bss_end);
+	pr_info("mm_set: 0x%x - 0x%x\n",mm_set_start, mm_set_end);
+	pr_info("heap start: 0x%x\n", heap_start);
 	while(1) {
 		char c = get_c();
 		pr_info("key: 0x%x\n", c);
