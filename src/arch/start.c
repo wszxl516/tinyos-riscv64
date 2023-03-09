9c4dc78ee70e42133389bbc71b3a936fe70d4ad2
Zhang <wszxl516@163.com>
2023-03-09 22:53:47 +0800

    traps and interrupt.

--- src/arch/start.c
+++ src/arch/start.c
@@ -1,18 +1,9 @@
 
+#include "start.h"
 #include "cpu.h"
 #include "printf.h"
 //riscv-privileged-20211203-2.pdf
-extern void main(void);
-extern void trap_handler();
-static inline void clear_bss(){
-    __asm__("la t5, bss_start\n"
-            "la t6, bss_end\n"
-            "bss_clear:\n"
-            "sd zero, (t5)\n"
-            "addi t5, t5, 8\n"
-            "bgeu t5, t6, bss_clear\n"
-            );
-}
+
 
 void handle_trap() {
     usize mcause = REG_READ_P(mcause), mepc = REG_READ_P(mepc);
@@ -23,86 +14,84 @@ void handle_trap() {
     {
         switch (exception_code)
         {
-        case 1:
+        case SUPERVISOR_SOFTWARE_INTERRUPT:
             pr_err("Supervisor software interrupt.");
             break;
-        case 3:
+        case MACHINE_SOFTWARE_INTERRUPT:
             pr_err("Machine software interrupt.");
             break;
-        case 5:
+        case SUPERVISOR_TIMER_INTERRUPT:
             pr_err("Supervisor timer interrupt.");
             break;
-        case 7:
+        case MACHINE_TIMER_INTERRUPT:
             pr_err("Machine timer interrupt.");
             break;
-        case 9:
+        case SUPERVISOR_EXTERNAL_INTERRUPT:
             pr_err("Supervisor external interru.");
             break;
-        case 11:
+        case MACHINE_EXTERNAL_INTERRUPT:
             pr_err("Machine external interrupt.");
             break;
         default:
-            pr_err("Designated for platform us");
+            pr_err("Unknown Exception.");
             break;
         }
     }else
+    //Exception
     {
         switch (exception_code)
         {
-        case 0:
+        case INSTRUCTION_ADDRESS_MISALIGNED:
             pr_err("Instruction address misaligned.");
             break;
-        case 1:
+        case INSTRUCTION_ACCESS_FAULT:
             pr_err("Instruction access fault.");
             break;
-        case 2:
+        case ILLEGAL_INSTRUCTION:
             pr_err("Illegal instruction.");
             break;
-        case 3:
+        case BREAKPOINT:
             pr_err("Breakpoint.");
             break;        
-        case 4:
+        case LOAD_ADDRESS_MISALIGNED:
             pr_err("Load address misaligned.");
             break;  
-        case 5:
+        case LOAD_ACCESS_FAULT:
             pr_err("Load access fault.");
             break;  
-        case 6:
+        case STORE_AMO_ADDRESS_MISALIGNED:
             pr_err(" Store/AMO address misaligned.");
             break;        
-        case 7:
+        case STORE_AMO_ACCESS_FAULT:
             pr_err("Store/AMO access fault.");
             break;
-        case 8:
+        case ENVIRONMENT_CALL_FROM_U_MODE:
             pr_err("Environment call from U-mode.");
             break;        
-        case 9:
+        case ENVIRONMENT_CALL_FROM_S_MODE:
             pr_err("Environment call from S-mode.");
             break;        
         //Reserved     
-        case 11:
+        case ENVIRONMENT_CALL_FROM_M_MODE:
             pr_err("Environment call from M-mode.");
             break;        
-        case 12:
+        case INSTRUCTION_PAGE_FAULT:
             pr_err("Instruction page fault.");
             break;
-        case 13:
+        case LOAD_PAGE_FAULT:
             pr_err("Load page fault.");
             break;     
         //14 Reserved  
-        case 15:
+        case STORE_AMO_PAGE_FAULT:
             pr_err("Store/AMO page fault.");
             break; 
         //6–23 Reserved       
         case 24 ... 31:
-            pr_err("Designated for custom use.");
-            break;
         //32–47 Reserved      
         case 48 ... 63:
-            pr_err("Designated for custom use.");
-            break;
         //0 ≥64 Reserveds
         default:
+            pr_err("Unknown Exception.");
             break;
         }
     }
@@ -111,12 +100,12 @@ void handle_trap() {
     REG_WRITE_P(mepc, (mepc + 0x4));
 }
 void start(){
-    /*set thread pointer to 0*/
+    //set thread pointer to 0
     REG_WRITE_G(tp, 0);
-    /*set MPP to 1 (supervisor mode) */ 
-    /*mstatus.MIE = Supervisor*/
+    //set MPP to 1 (supervisor mode)
+    //mstatus.MIE = Supervisor
     REG_WRITE_P(mstatus, (0b1 << 11));
-    /* Reset satp  disable mmu*/
+    // Reset satp  disable mmu
     set_mmu(MMU_BARE, 0, 0);
     // physical memory protection
     REG_WRITE_P(pmpcfg0,0xf);
@@ -126,13 +115,12 @@ void start(){
     REG_WRITE_P(mideleg, 0xffff);
     // keep each CPU's hartid in its tp register, for cpuid().
     REG_WRITE_G(tp, REG_READ_P(mhartid));
-    // /* enable fpu*/
+    // enable fpu
     REG_WRITE_P(sstatus, (0b1 << 13));
-
+    // set trap handler
     REG_WRITE_P(mtvec, (usize)trap_handler);
     /*machine mode exception*/
     REG_WRITE_P(mepc, (usize)main);
-    //clear_bss();
+    CLEAR_BSS();
     main();
-    asm volatile("mret");
 }
